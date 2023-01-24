bcf08d5b2376c86eeb6ee2c8396b7a43f041f5f0
kn201dvv <105391110+kn201dvv@users.noreply.github.com>
2023-01-24 13:32:25 +0200

    Update app.spec.js

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -2,18 +2,15 @@ const {test, expect} = require ('@playwright/test')
 const yaml = require('js-yaml')
 const fs = require('fs')
 const config = yaml.load(fs.readFileSync('./tests/info.yml', 'utf8'));
-const emailField = page.locator('input[name="email"]');
-const passField = page.locator('input[name="pass"]');
-
 
 test('log',async ({page})=>{
     await page.goto('https://m.facebook.com/login/?locale2=ru_RU');
-    await  emailField.click();
-    await  emailField.press('Control+a');
-    await  emailField.fill(config.login);
-    await  passField.click();
-    await  passField.press('Control+a');
-    await  passField.fill(config.password);
+    await  page.locator('input[name="email"]').click();
+    await  page.locator('input[name="email"]').press('Control+a');
+    await  page.locator('input[name="email"]').fill(config.login);
+    await  page.locator('input[name="pass"]').click();
+    await  page.locator('input[name="pass"]').press('Control+a');
+    await  page.locator('input[name="pass"]').fill(config.password);
     await  page.locator('button[name="login"]').click();
 })
 
