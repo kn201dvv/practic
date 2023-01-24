5ccb7f09591b6c5fce4e2ee7104f2e2e6c7157a7
kn201dvv <105391110+kn201dvv@users.noreply.github.com>
2023-01-24 13:26:03 +0200

    F! login

---
+++ tests/app.spec.js
@@ -0,0 +1,20 @@
+const {test, expect} = require ('@playwright/test')
+const yaml = require('js-yaml')
+const fs = require('fs')
+const config = yaml.load(fs.readFileSync('./tests/info.yml', 'utf8'));
+const emailField = page.locator('input[name="email"]');
+const passField = page.locator('input[name="pass"]');
+
+
+test('log',async ({page})=>{
+    await page.goto('https://m.facebook.com/login/?locale2=ru_RU');
+    await  emailField.click();
+    await  emailField.press('Control+a');
+    await  emailField.fill(config.login);
+    await  passField.click();
+    await  passField.press('Control+a');
+    await  passField.fill(config.password);
+    await  page.locator('button[name="login"]').click();
+})
+
+
